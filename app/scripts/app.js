0a43cbef5f79775816acfd825548c101cbe1953e
Paul Parton <paul@paulparton.com>
2013-10-08 23:50:00 +1100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('communityDirectoryApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var mainController = app.controller('MainCtrl', function ($scope) {
+
+});
